5821752e1a05bce8c51e196bfecb9faf5a1af84a
smit-sk <patelsmit7008@gmail.com>
2024-02-19 09:21:08 -0500

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
